eb1dfaa1bf8a230b44e2eca9215dba62eb0bb872
mtreerungroj <m.treerungroj@gmail.com>
2018-02-18 13:54:05 +0700

    get real data

--- src/api.js
+++ src/api.js
@@ -1,46 +1,52 @@
+import axios from 'axios'
+
 const BookAPI = {
-  getNames: function() {
-    return [
-      {
-        list_name: "Combined Print and E-Book Fiction",
-        display_name: "Combined Print & E-Book Fiction",
-        list_name_encoded: "combined-print-and-e-book-fiction"
-      },
-      {
-        list_name: "Combined Print and E-Book Nonfiction",
-        display_name: "Combined Print & E-Book Nonfiction",
-        list_name_encoded: "combined-print-and-e-book-nonfiction"
-      }
-    ];
+  apiKey: '73b19491b83909c7e07016f4bb4644f9:2:60667290',
+  getNames: function () {
+    return axios // return promise, who calls this will get data by .then()
+      .get(
+        `http://api.nytimes.com/svc/books/v3/lists/names.json?api-key=${this.apiKey}`
+      )
+      .then(response => response.data.results)
+    // return [
+    //   {
+    //     list_name: 'Combined Print and E-Book Fiction',
+    //     display_name: 'Combined Print & E-Book Fiction',
+    //     list_name_encoded: 'combined-print-and-e-book-fiction'
+    //   },
+    //   {
+    //     list_name: 'Combined Print and E-Book Nonfiction',
+    //     display_name: 'Combined Print & E-Book Nonfiction',
+    //     list_name_encoded: 'combined-print-and-e-book-nonfiction'
+    //   }
+    // ]
   },
-  getBooks: function(list_name_encoded) {
+  getBooks: function (list_name_encoded) {
     return [
       {
         rank: 1,
-        primary_isbn13: "9781250165619",
+        primary_isbn13: '9781250165619',
         publisher: "St. Martin's",
-        description:
-          "A former prisoner of war returns from Vietnam and moves his family to Alaska, where they face tough conditions.",
-        title: "THE GREAT ALONE",
-        author: "Kristin Hannah",
-        book_image: "https://s1.nyt.com/du/books/images/9781250165619.jpg",
+        description: 'A former prisoner of war returns from Vietnam and moves his family to Alaska, where they face tough conditions.',
+        title: 'THE GREAT ALONE',
+        author: 'Kristin Hannah',
+        book_image: 'https://s1.nyt.com/du/books/images/9781250165619.jpg',
         book_image_width: 326,
         book_image_height: 495
       },
       {
         rank: 2,
-        primary_isbn13: "9781616207601",
-        publisher: "Algonquin",
-        description:
-          "A newlywed couple's relationship is tested when the husband is sentenced to 12 years in prison.",
-        title: "AN AMERICAN MARRIAGE",
-        author: "Tayari Jones",
-        book_image: "https://s1.nyt.com/du/books/images/9781616207601.jpg",
+        primary_isbn13: '9781616207601',
+        publisher: 'Algonquin',
+        description: "A newlywed couple's relationship is tested when the husband is sentenced to 12 years in prison.",
+        title: 'AN AMERICAN MARRIAGE',
+        author: 'Tayari Jones',
+        book_image: 'https://s1.nyt.com/du/books/images/9781616207601.jpg',
         book_image_width: 330,
         book_image_height: 495
       }
-    ];
+    ]
   }
-};
+}
 
-export default BookAPI;
+export default BookAPI
