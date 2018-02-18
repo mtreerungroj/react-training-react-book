5c259e6c25207451983005624c4d032305b59a21
mtreerungroj <m.treerungroj@gmail.com>
2018-02-18 14:43:01 +0700

    can search for name

--- src/components/NamePage.js
+++ src/components/NamePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Field, Control, Input } from 'bloomer'
+import _ from 'lodash'
 import BookAPI from '../api'
 
 const NameItem = ({ list_name, list_name_encoded, match }) => (
@@ -22,13 +24,38 @@ class NamePage extends Component {
 
     ;(async () => {
       let names = await BookAPI.getNames()
+      this.allNames = names
       this.setState({ names })
     })()
+  }
+
+  handleSearchTextChange = e => {
+    let searchText = e.target.value
+    let names = this.state.name
+    if (searchText) {
+      names = _.filter(
+        this.allNames,
+        n => n.list_name.search(new RegExp(searchText, 'i')) >= 0
+      )
+      this.setState({ names })
+    } else {
+      this.setState({ names: this.allNames })
+    }
   }
 
   render () {
     return (
       <div>
+        <Field>
+          <Control>
+            <Input
+              type='text'
+              placeholder='Search'
+              onChange={this.handleSearchTextChange}
+              style={{ width: 400 }}
+            />
+          </Control>
+        </Field>
         {this.state.names.map(n => (
           <NameItem {...n} match={this.props.match} key={n.list_name_encoded} />
         ))}
