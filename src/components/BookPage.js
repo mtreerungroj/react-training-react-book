ef506ef57b7678792f4a283041705874a02dcae3
mtreerungroj <m.treerungroj@gmail.com>
2018-02-18 10:51:02 +0700

    show book name

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import BookAPI from "../api";
+
+const BookItem = ({ title }) => <div>{title}</div>;
 
 class BookPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { books: [] };
+  }
+
+  componentDidMount() {
+    let books = BookAPI.getBooks(this.props.match.params.list_name_encoded);
+    this.setState({ books });
+  }
+
   render() {
-    return <div>Book</div>;
+    return (
+      <div>
+        <div>Book from {this.props.match.params.list_name_encoded}</div>
+        {this.state.books.map(b => <BookItem {...b} key={b.primary_isbn13} />)}
+      </div>
+    );
   }
 }
 
