0cd4ddbba1914eae1847de6987b84e0955a373f5
mtreerungroj <m.treerungroj@gmail.com>
2018-02-18 10:05:30 +0700

    get data from api

--- src/components/NamePage.js
+++ src/components/NamePage.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import BookAPI from "../api";
+
+const NameItem = ({ list_name }) => <div>{list_name}</div>;
 
 class NamePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { names: [] };
+  }
+
+  componentDidMount() {
+    let names = BookAPI.getNames();
+    this.setState({ names });
+  }
+
   render() {
-    return (
-      <div>
-        Name
-      </div>
-    );
+    return <div>{this.state.names.map(n => <NameItem {...n} />)}</div>;
   }
 }
 
