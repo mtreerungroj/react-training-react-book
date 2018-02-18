4285c9f2c2f09855e0540396e28359f26cba1294
mtreerungroj <m.treerungroj@gmail.com>
2018-02-18 11:00:37 +0700

    render isbn1 when click book

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import BookAPI from "../api";
 
-const BookItem = ({ title }) => <div>{title}</div>;
+const BookItem = ({ title, primary_isbn13, match }) => (
+  <div>
+    <Link to={`${match.url}/${primary_isbn13}`}>{title}</Link>
+  </div>
+);
+
+const BookDetail = ({ match }) => <div>{match.params.primary_isbn13}</div>;
 
 class BookPage extends Component {
   constructor(props) {
@@ -18,7 +25,13 @@ class BookPage extends Component {
     return (
       <div>
         <div>Book from {this.props.match.params.list_name_encoded}</div>
-        {this.state.books.map(b => <BookItem {...b} key={b.primary_isbn13} />)}
+        {this.state.books.map(b => (
+          <BookItem {...b} key={b.primary_isbn13} match={this.props.match} />
+        ))}
+        <Route
+          path={`${this.props.match.url}/:primary_isbn13`}
+          render={BookDetail}
+        />
       </div>
     );
   }
